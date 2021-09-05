ddbce0e71c077d6bef2f973af627a1e4b967bc81
heraldOfMechanus <jccallahan11@gmail.com>
2021-09-05 16:31:24 -0500

    Cleaned up the Error Message (WIP)

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,12 +1,30 @@
 import {Alert} from '@material-ui/lab';
+import {Typography} from '@material-ui/core';
+import {makeStyles, useTheme} from '@material-ui/core/styles';
 
 interface IErrorMessageProps {
     errorMessage: string;
 }
 
 function ErrorMessageComponent(props: IErrorMessageProps) {
+
+    const useStyles = makeStyles((theme) => ({
+        alert: {
+            display: 'flex',
+            alignSelf: 'center',
+            padding: theme.spacing(0, 5),
+            width: `25rem`,
+            margin: 54,
+        },
+    }));
+
+    const classes = useStyles();
+    const theme = useTheme();
+
     return(
-        <Alert severity="error">{props.errorMessage}</Alert>
+        <Alert className={classes.alert} severity="error">
+            <Typography>{props.errorMessage}</Typography>
+        </Alert>
     )
 }
 
