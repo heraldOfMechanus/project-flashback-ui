07f09c297c832d30f6ddac56e95ee8f1f56326be
heraldOfMechanus <jccallahan11@gmail.com>
2021-09-11 16:46:40 -0500

    Committing threadservice

--- src/remote/thread-service.ts
+++ src/remote/thread-service.ts
@@ -10,3 +10,13 @@ export const getAllThreads = async (req: {subforumId: string}) => {
 
     return resp.data;
 }
+
+export const addNewThread = async(req: {id: string, userId: string, subforumId: string, threadTitle: string, threadContent: string}) => {
+    let resp = await flashbackClient.post('forum/create-thread', req)
+
+    if(resp.status >= 400 && resp.status <= 599) {
+        throw resp.data;
+    }
+
+    return resp.data;
+}
