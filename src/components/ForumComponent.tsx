ccddd92d9b2894d7af0d1fbd9a029f5954b3b119
heraldOfMechanus <jccallahan11@gmail.com>
2021-09-11 19:59:04 -0500

    modal deployed

--- src/components/ForumComponent.tsx
+++ src/components/ForumComponent.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
-import { Container, Typography, CssBaseline, Grid, makeStyles } from '@material-ui/core';
+import { Container, Modal, useTheme, Typography, CssBaseline, Grid, makeStyles } from '@material-ui/core';
 import { ButtonBase } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { Subforum } from '../dtos/Subforum';
 import { getAllThreads } from '../remote/thread-service';
 import { Thread } from '../dtos/Thread';
+import { SettingsPowerRounded } from '@mui/icons-material';
 
 interface IForumProps {
     currentTopic: Subforum | undefined
@@ -14,13 +15,25 @@ interface IForumProps {
 
 function ForumComponent(props: IForumProps) {
 
-    let [threads, setThreads] = useState([] as Thread[]);
+    const [threads, setThreads] = useState([] as Thread[]);
+    const [open, setOpen] = useState(false);
+    const [done, setDone] = useState(false);
     
     function showState() {
         console.log(props.currentTopic);
     }
 
-    const useStyles = makeStyles(() => ({
+    function getModalStyle() {
+        return {
+          top: '30%',
+          left: '34%',
+        };
+    }
+    
+    const modalStyle = getModalStyle();
+    const theme = useTheme();
+
+    const useStyles = makeStyles((theme) => ({
         root: {
             backgroundColor: 'lavender',
             width: '80%',
@@ -29,13 +42,30 @@ function ForumComponent(props: IForumProps) {
             backgroundColor: 'lightskyblue',
             width: '15rem',
         },
+        paper: {
+            position: 'absolute',
+            width: 400,
+            backgroundColor: theme.palette.background.paper,
+            boxShadow: theme.shadows[5],
+            padding: theme.spacing(2, 4, 3),
+        }
     }))
 
     const classes = useStyles();
 
+    const body = (
+        <div style={modalStyle} className={classes.paper}>
+          <h2 id="simple-modal-title">Text in a modal</h2>
+          <p id="simple-modal-description">
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </p>
+        </div>
+    );
+
     useEffect(() => {
-        if(!threads[0]) {
+        if(!done) {
             fetchThreads();
+            setDone(true);
         }
     })
 
@@ -50,11 +80,32 @@ function ForumComponent(props: IForumProps) {
         }
     }
 
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     //#TODO: Button/Modal that will allow the user to create new threads on this subforum!
     // Display the threads from the database matching this topic
     return (
         <>
             <CssBaseline/>
+            <div>
+                <button type="button" onClick={handleOpen}>
+                    Open Modal
+                </button>
+                <Modal
+                    open={open}
+                    onClick={handleClose}
+                    aria-labelledby="simple-modal-title"
+                    aria-describedby="simple-modal-description"
+                >
+                    {body}
+                </Modal>
+            </div>
             <Container className={classes.root} maxWidth='lg'>
                 <Typography variant='h1'>{props.currentTopic?.subforumTitle} Forum</Typography>
                 <Grid
