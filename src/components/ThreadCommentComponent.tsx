705610b256d144eadfda2d9866b6684cc450b13b
Teldu <spsmithsj@gmail.com>
2021-09-17 09:31:14 -0700

    fixed duplicate comment bug

--- src/components/ThreadCommentComponent.tsx
+++ src/components/ThreadCommentComponent.tsx
@@ -90,6 +90,8 @@ function ThreadCommentComponent(props: ICommentProps) {
         } catch (e: any) {
             setErrorMessage(e.message);
         }
+        setNewComment('');
+        
     }
 
     async function fetchComments() {
