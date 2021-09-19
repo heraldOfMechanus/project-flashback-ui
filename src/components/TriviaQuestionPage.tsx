3c622a54eb411ddc99156498d3dc1a24183be78e
metteec2 <chmettee@gmail.com>
2021-09-19 16:34:07 -0400

    Fixed bug

--- src/components/TriviaQuestionPage.tsx
+++ src/components/TriviaQuestionPage.tsx
@@ -31,9 +31,9 @@ function QuestionPage( props: ITriviaQuestionPage){
 
     const history = useHistory();
 
-    let currentSelected = -1;
+    let [currentSelected, setCurrentSelected] = useState(-1);
 
-    let [total,setTotal] =useState(0)
+    let [total,setTotal] = useState(0)
 
     let [x,setX] = useState(0)
 
@@ -102,6 +102,7 @@ function QuestionPage( props: ITriviaQuestionPage){
         setButtonColor2(false);
         setButtonColor3(false);
         setButtonColor4(false);
+        console.log(correctAnswer + " " + selectedAnswer + " " + points);
         if(correctAnswer === selectedAnswer){
             console.log("true")
             add(Number(points))
@@ -111,27 +112,25 @@ function QuestionPage( props: ITriviaQuestionPage){
     }
 
     function updateAnswer(a: number){
-        currentSelected = a;
 
+        setCurrentSelected(a);
+        
         if(a === 0) {
             setButtonColor1(true);
             setButtonColor2(false);
             setButtonColor3(false);
             setButtonColor4(false);
-        }
-        if(a === 1) {
+        } else if (a === 1) {
             setButtonColor1(false);
             setButtonColor2(true);
             setButtonColor3(false);
             setButtonColor4(false);
-        }
-        if(a === 2) {
+        } else if(a === 2) {
             setButtonColor1(false);
             setButtonColor2(false);
             setButtonColor3(true);
             setButtonColor4(false);
-        }
-        if(a === 3) {
+        } else if(a === 3) {
             setButtonColor1(false);
             setButtonColor2(false);
             setButtonColor3(false);
@@ -152,9 +151,6 @@ function QuestionPage( props: ITriviaQuestionPage){
     function updateX(){
         x+=1
         setX(x)
-        console.log(Cards.length)
-
-        updateAnswer(-1);
     }
 
     const [buttonColor1, setButtonColor1] = useState(false);
@@ -236,7 +232,7 @@ function QuestionPage( props: ITriviaQuestionPage){
                     <Button id="button-4" className={classes.buttons} onClick={ () =>{updateAnswer(3)}} variant="contained" color={buttonColor4 ? "primary" : "default"}>{Cards.answers[3]}</Button>
                     <br/>
 
-                    <Button className={classes.buttons} onClick={ () =>{isAnswerCorrect(Cards.correctAnswer, Cards.answers[currentSelected], Cards.points); updateX()}} variant="contained" color="primary">
+                    <Button className={classes.buttons} onClick={ () =>{isAnswerCorrect(Cards.correctAnswer, Cards.answers[currentSelected], Cards.points); updateX(); setCurrentSelected(-1)}} variant="contained" color="primary">
                         next question
                     </Button>
                     <br/>
